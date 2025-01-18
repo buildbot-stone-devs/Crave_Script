145e39cd03a169eb330e61fa38f41973831498db
Khnome <75486623+Khnome@users.noreply.github.com>
2025-01-18 09:26:16 +0200

    Adapt for org

---
+++ yaap.sh
@@ -0,0 +1,43 @@
+
+#!/bin/bash
+
+rm -rf .repo/local_manifests/
+
+# Rom source repo
+repo init -u https://github.com/yaap/manifest.git -b fifteen --git-lfs
+echo "=================="
+echo "Repo init success"
+echo "=================="
+
+# Clone local_manifests repository
+git clone -b yaap-a15 https://github.com/Yaap-stone-devs//local_manifests.git .repo/local_manifests
+echo "============================"
+echo "Local manifest clone success"
+echo "============================"
+
+# Sync the repositories
+/opt/crave/resync.sh
+echo "============================"
+
+# Cloning custom apps
+#rm -rf packages/apps/Settings
+#git clone https://github.com/Yaap-stone-devs//yaap_packages_apps_Settings.git packages/apps/Settings
+#echo "Clone success"
+
+# Export
+export BUILD_USERNAME=KhnomexMayuresh
+export BUILD_HOSTNAME=crave
+# export BUILD_BROKEN_DISABLE_BAZEL_SANDBOXING=1
+export TZ="Europe/Vilnius"
+echo "======= Export Done ======"
+
+# Set up build environment
+source build/envsetup.sh
+echo "====== Envsetup Done ======="
+
+# Lunch
+lunch yaap_stone-ap4a-userdebug
+echo "============="
+
+# Build rom
+m yaap
