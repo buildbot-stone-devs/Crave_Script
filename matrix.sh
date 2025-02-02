31ff4868812c6bc7507ae88d9662dd55372a5751
Mayuresh Nanal <92848611+Mayuresh2543@users.noreply.github.com>
2025-02-02 13:41:14 +0530

    Create matrix.sh

---
+++ matrix.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+rm -rf .repo/local_manifests/
+
+# Rom source repo
+repo init -u https://github.com/ProjectMatrixx/android.git -b 14.0 --git-lfs
+echo "=================="
+echo "Repo init success"
+echo "=================="
+
+# Clone local_manifests repository
+git clone -b matrix-a14 https://github.com/Yaap-stone-devs//local_manifests.git .repo/local_manifests
+echo "============================"
+echo "Local manifest clone success"
+echo "============================"
+
+# Sync the repositories
+/opt/crave/resync.sh
+echo "============================"
+
+# Export
+export BUILD_USERNAME=KhnomexMayuresh
+export BUILD_HOSTNAME=crave
+# export BUILD_BROKEN_DISABLE_BAZEL_SANDBOXING=1
+export TZ="Europe/Vilnius"
+echo "======= Export Done ======"
+
+# Set up build environment
+source build/envsetup.sh
+echo "====== Envsetup Done ======="
+
+# Brunch
+brunch stone
+echo "============="
+
