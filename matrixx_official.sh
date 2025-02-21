8b3d3d4161b680d9560524b9ed9cb253f8d0d07e
Mayuresh Nanal <92848611+Mayuresh2543@users.noreply.github.com>
2025-02-21 09:41:56 +0530

    Create matrixx_official.sh

---
+++ matrixx_official.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+rm -rf .repo/local_manifests/
+
+# Rom source repo
+repo init -u https://github.com/ProjectMatrixx/android.git -b 15.0 --git-lfs
+echo "=================="
+echo "Repo init success"
+echo "=================="
+
+# Clone local_manifests repository
+git clone -b matrix-a15-official https://github.com/Yaap-stone-devs/local_manifests.git .repo/local_manifests
+echo "============================"
+echo "Local manifest clone success"
+echo "============================"
+
+# Sync the repositories
+/opt/crave/resync.sh
+echo "============================"
+
+# Export
+export BUILD_USERNAME=MayureshxKhnome
+export BUILD_HOSTNAME=crave
+export TZ="Europe/Vilnius"
+echo "======= Export Done ======"
+
+# Sign
+cd vendor/lineage-priv/keys
+chmod +x ./syslink.sh
+././syslink.sh
+cd ../../..
+echo "Signing success"
+
+# Set up build environment
+. build/envsetup.sh
+echo "====== Envsetup Done ======="
+
+# Brunch
+brunch stone
+echo "============="
