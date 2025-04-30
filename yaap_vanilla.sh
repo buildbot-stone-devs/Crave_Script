a556a237cdd89c0725c1d44eed0a7c150694db3b
Mayuresh Nanal <92848611+Mayuresh2543@users.noreply.github.com>
2025-04-30 23:21:17 +0530

    Update yaap_vanilla.sh

--- yaap_vanilla.sh
+++ yaap_vanilla.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
 rm -rf .repo/local_manifests/
-rm -rf device/xiaomi/stone
-rm -rf vendor/xiaomi/stone
 
 # Rom source repo
 repo init -u https://github.com/yaap/manifest.git -b fifteen --git-lfs
@@ -11,17 +9,15 @@ echo "Repo init success"
 echo "=================="
 
 # Clone local_manifests repository
-git clone -b yaap-a15-vanilla https://github.com/Yaap-stone-devs/local_manifests.git .repo/local_manifests
-echo "============================"
-echo "Local manifest clone success"
-echo "============================"
+#git clone -b yaap-a15-vanilla https://github.com/Yaap-stone-devs/local_manifests.git .repo/local_manifests
+#echo "============================"
+#echo "Local manifest clone success"
+#echo "============================"
 
 # Sync the repositories
 /opt/crave/resync.sh
 echo "============================"
 
-git clone https://github.com/buildbot-stone-devs/vendor_xioami_stone_yaap_new.git vendor/xiaomi/stone
-git clone https://github.com/Mayuresh2543/device_xiaomi_stone_yaap.git device/xiaomi/stone
 # Export
 export BUILD_USERNAME=MayureshXKhnome
 export BUILD_HOSTNAME=crave
@@ -35,8 +31,6 @@ echo "====== Envsetup Done ======="
 # Lunch
 lunch yaap_stone-eng
 echo "============="
-
-make clean
 
 # Build rom
 m yaap
